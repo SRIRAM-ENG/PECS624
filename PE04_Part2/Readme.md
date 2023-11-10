12e468f03554e0e95710b5d6c3a159eb50668456
SRIRAM-ENG <147373935+SRIRAM-ENG@users.noreply.github.com>
2023-11-10 01:14:40 +0000

    PE04_Part2Submission_Sriram

---
+++ PE04_Part2/Readme.md
@@ -0,0 +1,6 @@
+Input:
+The code defines a React Native application  that renders a list of profile cards.
+Process:
+The handleProfileCardPress method toggles the visibility of a thumbnail image in each profile card upon pressing.
+Output:
+The application displays a grid of profile cards, each containing a user image, name, occupation, and description.
--- PE04_Part2/Styling/App.js
+++ PE04_Part2/Styling/App.js
@@ -1,35 +1,16 @@
-import React, { Component } from 'react'; 
 
-// PropTypes lets you specify what properties the ProfileCard component can accept. 
+import React, { Component } from 'react';
 
-import PropTypes from 'prop-types';  
+import PropTypes from 'prop-types'; 
 
- 
+import update from 'immutability-helper'; 
 
-// The immutability helper function update lets you update a specific piece of the component’s state. 
+import { Platform, Image, StyleSheet, Text, View, TouchableHighlight } from 'react-native'; 
 
-// npm install immutability-helper --save 
+const userImage = require('./assets/icons8-user-64.png');
 
-import update from 'immutability-helper';  
-
- 
-
-/* 
-
-  Imports the Platform utility component to programmatically select styles based on the platform. 
-
-*/ 
-
-import { Platform, Image, StyleSheet, Text, View, TouchableHighlight } from 'react-native';  
-
- 
-
-const userImage = require('./assets/icons8-user-64.png'); 
-
- 
-
-const data = [{ 
-
+const data = [{
+    image: userImage,
     image: userImage, 
 
     name: 'Sriram Bairi', 
@@ -39,385 +20,236 @@ const data = [{
     description: 'Passionate about coding and creating awesome apps and looking for learning new skills set' + 'He loves using JS to build React Native applications ' + 'for iOS and Android', 
 
     showThumbnail: true 
+  },
+  {
+    image: userImage, 
 
-  } 
-
-]; 
-
- 
-
-const ProfileCard = (props) => { 
-
-  const { image, name, occupation, description, onPress, showThumbnail } = props; 
-
-  let containerStyles = [styles.cardContainer]; 
-
- 
-
-  if (showThumbnail) {   
-
-    containerStyles.push(styles.cardThumbnail); 
-
-  } 
-
- 
-
-  return ( 
-
-    <TouchableHighlight onPress={onPress}>  
-
-      <View style={[containerStyles]}> 
-
-        <View style={styles.cardImageContainer}> 
-
-          <Image style={styles.cardImage} source={image}/> 
-
-        </View> 
-
-        <View> 
-
-          <Text style={styles.cardName}> 
-
-            {name} 
-
-          </Text> 
-
-        </View> 
-
-        <View style={styles.cardOccupationContainer}> 
-
-          <Text style={styles.cardOccupation}> 
-
-            {occupation} 
-
-          </Text> 
-
-        </View> 
-
-        <View> 
-
-          <Text style={styles.cardDescription}> 
-
-            {description} 
-
-          </Text> 
-
-        </View> 
-
-      </View> 
-
-    </TouchableHighlight> 
-
-  ) 
-
-}; 
-
- 
-
-ProfileCard.propTypes = { 
-
-  image: PropTypes.number.isRequired, 
-
-  name: PropTypes.string.isRequired, 
-
-  occupation: PropTypes.string.isRequired, 
-
-  description: PropTypes.string.isRequired, 
-
-  showThumbnail: PropTypes.bool.isRequired, 
-
-  onPress: PropTypes.func.isRequired 
-
-}; 
-
- 
-
-export default class App extends Component { 
-
-  constructor(props, context) { 
-
-    super(props, context); 
-
-    this.state = {  
-
-      data: data 
-
-    } 
-
-  } 
-
- 
-
-  handleProfileCardPress = (index) => { 
-
-    const showThumbnail = !this.state.data[index].showThumbnail; 
-
-    this.setState({ 
-
-      data: update(this.state.data, {[index]: {showThumbnail: {$set: showThumbnail}}}) 
-
-    }); 
-
-  }; 
-
-   
-
-  render() { 
-
-    const list = this.state.data.map(function(item, index) {  
-
-      const { image, name, occupation, description, showThumbnail } = item; 
-
-      return <ProfileCard key={'card-' + index} 
-
-                   image={image} 
-
-                   name={name} 
-
-                   occupation={occupation} 
-
-                   description={description} 
-
-                   onPress={this.handleProfileCardPress.bind(this, index)} 
-
-                   showThumbnail={showThumbnail}/> 
-
-    }, this); 
-
- 
-
-    return ( 
-
-      <View style={styles.container}> 
-
-        {list}  
-
-      </View> 
-
-    ); 
-
-  } 
-
-} 
-
- 
-
-const profileCardColor = 'dodgerblue'; 
-
- 
-
-const styles = StyleSheet.create({ 
-
-    container: { 
-
-        flex: 1, 
-
-        justifyContent: 'center', 
-
-        alignItems: 'center' 
-
-    }, 
-
-    /* 
-
-      Adds a drop shadow to the card container based on the platform 
-
-    */ 
-
-    cardContainer: { 
-
-        alignItems: 'center', 
-
-        borderColor: 'black', 
-
-        borderWidth: 3, 
-
-        borderStyle: 'solid', 
-
-        borderRadius: 20, 
-
-        backgroundColor: profileCardColor, 
-
-        width: 300, 
-
-        height: 400, 
-
-        ...Platform.select({  
-
-          ios: { 
-
-            shadowColor: 'black', 
-
-            shadowOffset: { 
-
-              height: 10 
-
-            }, 
-
-            shadowOpacity: 1 
-
-          }, 
-
-          android: { 
-
-            elevation: 15 
-
-          } 
-
-        }) 
-
-    }, 
-
-    /* 
-
-      Adds a drop shadow to the circular image container 
-
-    */ 
-
-    cardImageContainer: { 
-
-        alignItems: 'center', 
-
-        backgroundColor: 'white', 
-
-        borderWidth: 3, 
-
-        borderColor: 'black', 
-
-        width: 120, 
-
-        height: 120, 
-
-        borderRadius: 60, 
-
-        marginTop: 30, 
-
-        paddingTop: 15, 
-
-        ...Platform.select({  
-
-          ios: { 
-
-            shadowColor: 'black', 
-
-            shadowOffset: { 
-
-              height: 10, 
-
-            }, 
-
-            shadowOpacity: 1 
-
-          }, 
-
-          android: { 
-
-            borderWidth: 3, 
-
-            borderColor: 'black', 
-
-            elevation: 15 
-
-          } 
-
-        }) 
-
-    }, 
-
-    cardImage: { 
-
-        width: 80, 
-
-        height: 80 
-
-    }, 
-
-    /* 
-
-        You can use the textShadowColor, textShadowOffset, and textShadowRadius properties to add a shadow to a Text element.  
-
-        To create a shadow, you need to specify three things: 
-
-            The color 
-
-            The offset 
-
-            The radius 
-
- 
-
-        The offset specifies the position of the shadow relative to the component casting the shadow.  
-
-        The radius basically defines how blurry the shadow appears. 
-
-    */ 
-
-    cardName: { 
-
-        color: 'white', 
-
-        fontWeight: 'bold', 
-
-        fontSize: 24, 
-
-        marginTop: 30, 
-
-        textShadowColor: 'black', 
-
-        textShadowOffset: { 
-
-            height: 5, 
-
-            width: 5 
-
-        }, 
-
-        textShadowRadius: 3  
-
-    }, 
-
-    cardOccupationContainer: { 
-
-        borderColor: 'black', 
-
-        borderBottomWidth: 3 
-
-    }, 
-
-    cardOccupation: { 
-
-        fontWeight: 'bold',  
-
-        marginTop: 10, 
-
-        marginBottom: 10, 
-
-    }, 
-
-    cardDescription: { 
-
-        fontStyle: 'italic',  
-
-        marginTop: 10, 
-
-        marginRight: 40, 
-
-        marginLeft: 40, 
-
-        marginBottom: 10 
-
-    }, 
-
-    /* 
-
-        The cardThumbnail style reduces the component’s size by 80%. 
-
-    */ 
-
-        cardThumbnail: { 
-
-          transform: [{scale: 0.2}] 
-
-      }, 
-
-}); 
+    name: 'Sriram Bairi', 
+
+    occupation: 'Analyst', 
+
+    description: 'Passionate about coding and creating awesome apps and looking for learning new skills set' + 'He loves using JS to build React Native applications ' + 'for iOS and Android', 
+
+    showThumbnail: true 
+  },
+  {
+    image: userImage, 
+
+    name: 'Sriram Bairi', 
+
+    occupation: 'Analyst', 
+
+    description: 'Passionate about coding and creating awesome apps and looking for learning new skills set' + 'He loves using JS to build React Native applications ' + 'for iOS and Android', 
+
+    showThumbnail: true 
+  },
+  {
+    image: userImage, 
+
+    name: 'Sriram Bairi', 
+
+    occupation: 'Analyst', 
+
+    description: 'Passionate about coding and creating awesome apps and looking for learning new skills set' + 'He loves using JS to build React Native applications ' + 'for iOS and Android', 
+
+    showThumbnail: true 
+  },
+  {
+    image: userImage, 
+
+    name: 'Sriram Bairi', 
+
+    occupation: 'Analyst', 
+
+    description: 'Passionate about coding and creating awesome apps and looking for learning new skills set' + 'He loves using JS to build React Native applications ' + 'for iOS and Android', 
+
+    showThumbnail: true 
+  },
+  {
+    image: userImage, 
+
+    name: 'Sriram Bairi', 
+
+    occupation: 'Analyst', 
+
+    description: 'Passionate about coding and creating awesome apps and looking for learning new skills set' + 'He loves using JS to build React Native applications ' + 'for iOS and Android', 
+
+    showThumbnail: true 
+  },
+];
+
+const ProfileCard = (props) => {
+  const { image, name, occupation, description, onPress, showThumbnail } = props;
+  let containerStyles = [styles.cardContainer];
+
+  if (showThumbnail) {
+    containerStyles.push(styles.cardThumbnail);
+  }
+
+  return (
+    <TouchableHighlight onPress={onPress}>
+      <View style={[containerStyles, props.style]}>
+        <View style={styles.cardImageContainer}>
+          <Image style={styles.cardImage} source={image} />
+        </View>
+        <View>
+          <Text style={styles.cardName}>{name}</Text>
+        </View>
+        <View style={styles.cardOccupationContainer}>
+          <Text style={styles.cardOccupation}>{occupation}</Text>
+        </View>
+        <View>
+          <Text style={styles.cardDescription}>{description}</Text>
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+ProfileCard.propTypes = {
+  image: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  occupation: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  showThumbnail: PropTypes.bool.isRequired,
+  onPress: PropTypes.func.isRequired,
+};
+
+export default class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      data: data,
+    };
+  }
+
+  handleProfileCardPress = (index) => {
+    const showThumbnail = !this.state.data[index].showThumbnail;
+    this.setState({
+      data: update(this.state.data, {
+        [index]: { showThumbnail: { $set: showThumbnail } },
+      }),
+    });
+  };
+
+  render() {
+    const list = this.state.data.map(function (item, index) {
+        const { image, name, occupation, description, showThumbnail } = item;
+        return (
+            <ProfileCard
+            key={'card-' + index}
+            image={image}
+            name={name}
+            occupation={occupation}
+            description={description}
+            onPress={this.handleProfileCardPress.bind(this, index)}
+            showThumbnail={showThumbnail}
+            />
+        );
+    }, this);
+
+    return (
+        <View style={styles.container}>
+            {list}
+        </View>
+    );
+  }
+}
+
+const profileCardColor = 'dodgerblue';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    flex: 1,
+    maxHeight: '100%',
+    flexWrap: 'wrap',
+    alignContent: 'space-around',
+    justifyContent: 'center',
+  },
+  cardContainer: {
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 20,
+    backgroundColor: profileCardColor,
+    width: 170,
+    height: 270,
+    padding: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10,
+        },
+        shadowOpacity: 1,
+      },
+      android: {
+        elevation: 15,
+      },
+    }),
+  },
+  cardImageContainer: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    marginTop: 15,
+    paddingTop: 7.5,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 5,
+        },
+        shadowOpacity: 1,
+      },
+      android: {
+        borderWidth: 3,
+        borderColor: 'black',
+        elevation: 7.5,
+      },
+    }),
+  },
+  cardImage: {
+    width: 40,
+    height: 40,
+  },
+  cardName: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 10,
+    marginTop: 15,
+    textShadowColor: 'black',
+    textShadowOffset: {
+      height: 2,
+      width: 2,
+    },
+    textShadowRadius: 3,
+  },
+  cardOccupationContainer: {
+    borderColor: 'black',
+    borderBottomWidth: 3,
+  },
+  cardOccupation: {
+    fontWeight: 'bold',
+    fontSize: 10,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  cardDescription: {
+    fontStyle: 'italic',
+    marginTop: 10,
+    fontSize: 10,
+    marginRight: 10,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  cardThumbnail: {
+    transform: [{ scale: 0.2 }],
+  },
+});
