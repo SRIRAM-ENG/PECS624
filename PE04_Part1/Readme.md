27f15ef43b3a374253d6f8b3ea8bfb0bb1e7717d
SRIRAM-ENG <147373935+SRIRAM-ENG@users.noreply.github.com>
2023-11-10 00:06:48 +0000

    PE04_Part1 Submission_Sriram

---
+++ PE04_Part1/Readme.md
@@ -0,0 +1,12 @@
+Input:
+A React Native code snippet defining a class-based component named 'App' that renders a profile card containing user information. 
+
+Process:
+
+The 'render' method structures the JSX code, creating a profile card within a View component.
+A 'user' object is declared with hardcoded personal information.
+The JSX code displays the user's image and information within the profile card.
+Styles are applied using the 'StyleSheet.create' method, defining various style rules for the components.
+
+Output:
+The React Native component generates a visually appealing profile card with a circular user image, displaying the user's name, occupation, and profile description. 
--- PE04_Part1/Styling/App.js
+++ PE04_Part1/Styling/App.js
@@ -1,105 +1,86 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import { Image, StyleSheet, View, Text } from 'react-native';
 
-import { Image, StyleSheet, View} from 'react-native'; 
+export default class App extends Component {
+    render() {
+        
+        const user = {
+            name: 'SriramBairi',
+            occupation: 'Software Analyst',
+            profileDescription: 'Passionate about coding and creating awesome apps and looking for learning new skills set '
+        };
 
- 
+        return (
+            <View style={styles.container}>
+                <View style={styles.cardContainer}>
+                    <View style={styles.cardImageContainer}>
+                        <Image
+                            style={styles.cardImage}
+                            source={require('./assets/icons8-user-64.png')}
+                        />
+                    </View>
+                    {/* User Information */}
+                    <View style={styles.userInfoContainer}>
+                        <Text style={styles.userName}>{user.name}</Text>
+                        <Text style={styles.userOccupation}>{user.occupation}</Text>
+                        <Text style={styles.userDescription}>{user.profileDescription}</Text>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
 
-export default class App extends Component  { 
+const profileCardColor = 'dodgerblue';
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
 
-    render() { 
+    cardContainer: {
+        alignItems: 'center',
+        borderColor: 'black',
+        borderWidth: 3,
+        borderStyle: 'solid',
+        borderRadius: 20,
+        backgroundColor: profileCardColor,
+        width: 300,
+        height: 400
+    },
 
-        return ( 
+    cardImageContainer: {
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderWidth: 3,
+        borderColor: 'black',
+        width: 120,
+        height: 120,
+        borderRadius: 60,
+        marginTop: 30,
+        paddingTop: 15
+    },
+    cardImage: {
+        width: 80,
+        height: 80
+    },
 
-            <View style={styles.container}> 
-
-                <View style={styles.cardContainer}> 
-
-                    <View style={styles.cardImageContainer}> 
-
-                        <Image style={styles.cardImage}        
-
-                               source={require('./assets/icons8-user-64.png')}/> 
-
-                    </View> 
-
-                </View> 
-
-            </View> 
-
-        ); 
-
-    } 
-
-} 
-
- 
-
-const profileCardColor = 'dodgerblue'; 
-
-const styles = StyleSheet.create({ 
-
-  container: { 
-
-      flex: 1, 
-
-      justifyContent: 'center', 
-
-      alignItems: 'center' 
-
-  }, 
-
-  
-  cardContainer: { 
-
-      alignItems: 'center', 
-
-      borderColor: 'black', 
-
-      borderWidth: 3, 
-
-      borderStyle: 'solid', 
-
-      borderRadius: 20, 
-
-      backgroundColor: profileCardColor, 
-
-      width: 300, 
-
-      height: 400 
-
-  }, 
-
-  
-  cardImageContainer: { 
-
-      alignItems: 'center', 
-
-      backgroundColor: 'white', 
-
-      borderWidth: 3, 
-
-      borderColor: 'black', 
-
-      width: 120, 
-
-      height: 120, 
-
-      borderRadius: 60, 
-
-      marginTop: 30, 
-
-      paddingTop: 15  
-
-  }, 
-
-  cardImage: { 
-
-      width: 80, 
-
-      height: 80 
-
-  } 
-
-}); 
-
- 
+    userInfoContainer: {
+        marginTop: 20,
+        alignItems: 'center'
+    },
+    userName: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    userOccupation: {
+        fontSize: 18,
+        textDecorationLine: 'underline',
+        color: 'red'
+    },
+    userDescription: {
+        fontSize: 16,
+        marginTop: 10
+    }
+});
